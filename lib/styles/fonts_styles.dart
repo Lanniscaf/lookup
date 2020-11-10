6ff71a3281a78a608365789b2cb2d1127bf203d0
Lanniscaf <paolanderfederico@gmail.com>
2020-11-10 18:36:38 -0300

    fuentes del proyecto  - Actualmente solo esta para el de la tarjeta

---
+++ lib/styles/fonts_styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+const CREDITCARDTEXT = TextStyle(
+  fontSize: 20.0,
+  color: Colors.white,
+  fontWeight: FontWeight.w600,
+  letterSpacing: 0.6,
+  fontStyle: FontStyle.italic
+);
