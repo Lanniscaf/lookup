8d3e7a97c739b16ca7c170f31119753a2916c147
Lanniscaf <paolanderfederico@gmail.com>
2020-11-10 18:37:04 -0300

    Modelo para manejar entre el servicio, los valores y el ui

---
+++ lib/view-model/ccmodel_view.dart
@@ -0,0 +1,32 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class CCModelView extends ChangeNotifier {
+
+  String ccnumber = '1234 5678 9012 3456';
+  String brand    = 'Mastercard';
+  String type     = 'Debit';
+  String level    = 'Prepaid';
+  String bank     = 'SOCIETE MONETIQUE-TUNISIE';
+  String website  = 'Bank website';
+  String number   = 'Bank phone';
+  String bin      = '1234  56xx  xxxx  xxxx';
+  String country  = 'United States';
+  bool   valid    = true;
+
+
+  // CCModelView({
+  //   this.ccnumber,
+  //   this.brand,
+  //   this.type,
+  //   this.level,
+  //   this.bank,
+  //   this.website,
+  //   this.number,
+  //   this.bin,
+  //   this.country,
+  //   this.valid
+  // });
+
+}
