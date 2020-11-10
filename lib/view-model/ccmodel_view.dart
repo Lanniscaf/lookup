fca3a41ad8fbf9ace0e2461a8e9219cd79f94d87
Lanniscaf <paolanderfederico@gmail.com>
2020-11-10 19:05:59 -0300

    ?

--- lib/view-model/ccmodel_view.dart
+++ lib/view-model/ccmodel_view.dart
@@ -4,14 +4,14 @@ import 'package:flutter/cupertino.dart';
 
 class CCModelView extends ChangeNotifier {
 
-  String ccnumber = '1234 5678 9012 3456';
+  String ccnumber = '1234  5678  9012  3456';
   String brand    = 'Mastercard';
   String type     = 'Debit';
   String level    = 'Prepaid';
   String bank     = 'SOCIETE MONETIQUE-TUNISIE';
   String website  = 'Bank website';
   String number   = 'Bank phone';
-  String bin      = '1234  56xx  xxxx  xxxx';
+  String bin      = '123456';
   String country  = 'United States';
   bool   valid    = true;
 
